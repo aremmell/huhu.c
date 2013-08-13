ab4c7e6277ecee905de0ad7d469d4e526b9a4467
Ryan Lederman <ryan.lederman@kayako.com>
2013-08-12 21:23:08 -0600

    Initial commit

---
+++ huhu.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define uhuu }
+#define huhu int
+#define uhu ,
+#define hu argv
+#define hhu (
+#define huhuh main
+#define huhuhu argc
+#define uuhu return 0
+#define uh )
+#define huu char*
+#define huhuhuh {
+#define huhuhh printf
+#define hhuh ;
+#define huh []
+#define huhh "\n"
+
+huhu huhuh hhu huhu huhuhu uhu huu hu huh uh huhuhuh huhuhh hhu "huhuhu" huhh uh hhuh hhuh uuhu hhuh uhuu
+
